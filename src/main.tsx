2b82e50f553ab7c98f1d1d0d9699c1826e015303
Шамхан Товсултанов <shamkhan.tvs@mail.ru>
2024-12-08 17:49:46 +0300

    Настроен проект для GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
